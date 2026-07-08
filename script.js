8e9f2c04178694d5ce951c0212dcf2f4d7f8d370
Vu Trong Hoang <vutronghoang@MacBook-Pro-cua-Vu.local>
2026-07-08 09:00:52 +0700

    First version

---
+++ script.js
@@ -0,0 +1,23 @@
+function openPopup(){
+
+    document.getElementById("popup").style.display="block";
+
+}
+
+function closePopup(){
+
+    document.getElementById("popup").style.display="none";
+
+}
+
+window.onclick=function(event){
+
+    const popup=document.getElementById("popup");
+
+    if(event.target==popup){
+
+        popup.style.display="none";
+
+    }
+
+}
